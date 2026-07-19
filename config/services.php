19ff09d26ed8e554f5f777aafbeb90912da9ab4d
Serg Bugayov <serg.bugayov@gmail.com>
2026-07-19 22:33:16 +0300

    fix config/services.php

--- config/services.php
+++ config/services.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Corvet\LightSymfonyBundle\Controller\AdminEntityController;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $container): void {
@@ -17,4 +18,14 @@ return static function (ContainerConfigurator $container): void {
             '../src/CorvetLightSymfonyBundle.php',
             '../src/DependencyInjection/',
         ]);
+
+    $services
+        ->set(AdminEntityController::class)
+            // ->args([
+            //     service('twig'),
+            //     service('doctrine'),
+            //     service('profiler'),
+            // ])
+            // ->tag('controller.service_arguments')
+            ;
 };
